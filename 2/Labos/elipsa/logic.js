8306ca7c4dec854407df9e31bcbf6c3269b86c27
Joshua Fletcher <jfletcher20@student.foi.hr>
2024-10-24 12:38:52 +0200

    Implemented autoscaling as parameter instead of default behavior.

---
+++ 2/Labos/elipsa/logic.js
@@ -0,0 +1,76 @@
+const unit = 40;
+const xmin = -20, xmax = 20, ymin = -20, ymax = 20;
+
+window.onload = function() {
+
+    const canvas = document.getElementById("renderer");
+    const canvasHeightSlider = document.getElementById("canvas-height");
+    const canvasWidthSlider = document.getElementById("canvas-width");
+    const unitSlider = document.getElementById("unit");
+    const aFocusSlider = document.getElementById("focus-a");
+    const bFocusSlider = document.getElementById("focus-b");
+    if (!canvas) alert("Greška - nema platna!");
+
+    canvasHeightSlider.oninput = function() {
+        canvas.height = this.value;
+        canvas.width = canvasWidthSlider.value;
+        gks.initRenderer();
+        drawEllipsis();
+    }
+
+    canvasWidthSlider.oninput =  function() {
+        canvas.width = this.value;
+        canvas.height = canvasHeightSlider.value;
+        gks.initRenderer();
+        drawEllipsis();
+    }
+
+    var gks = new GKS2(canvas, xmin, xmax, ymin, ymax);
+    gks.unit = unitSlider.value;
+
+    /*
+
+        Zadatak 2
+
+        Nacrtajte elipsu s poluosima a = 4 i b = 2.
+        Koordinatni sustav zajedno s oznakama na koordinatnim osima postavite kako je prikazano na slici.
+        Parametarske jednadˇzbe elipse s poluosima a i b su x = a cost, y = b sin t pri ˇcemu je t ∈ [0, 2π]. 
+    
+        Koristite GKS klasu.
+
+    */
+
+    function drawEllipsis() {
+
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        gks.nacrtajKoordinatniSustav(false, false, true, 10000, 10000);
+
+        gks.koristiBoju("red");
+        const a = aFocusSlider.value, b = bFocusSlider.value;
+
+        for (var t = 0; t <= 2 * Math.PI; t += 0.01) {
+            var x = a * Math.cos(t);
+            var y = b * Math.sin(t);
+            var xNext = a * Math.cos(t + 0.01 <= 2 * Math.PI ? t + 0.01 : 2 * Math.PI);
+            var yNext = b * Math.sin(t + 0.01 <= 2 * Math.PI ? t + 0.01 : 2 * Math.PI);
+            gks.postaviNa(x, y);
+            gks.linijaDo(xNext, yNext);
+            gks.povuciLiniju();
+        }
+
+        gks.nacrtajSlova("Elipsa", 3, -4.2, 1, /*"purple"*/ "lavender");
+        
+    }
+
+    unitSlider.oninput = function() {
+        gks.unit = this.value;
+        drawEllipsis();
+    }
+
+    aFocusSlider.oninput = bFocusSlider.oninput = function() {
+        drawEllipsis();
+    }
+
+    drawEllipsis();
+
+}
---
+++ 2/gks2.js
@@ -0,0 +1,194 @@
+/*
+
+    1.4. Implementirajte klasu GKS (Globalni Koordinatni Sustav) koja omogućava crtanje u globalnim koordinatama sa sljedećim metodama:
+        
+        postaviNa(x, y) – postavlja "olovku" na poziciju (x, y) u globalnim koordinatama (ne zaboraviti beginPath());
+        
+        linijaDo(x, y) – povlači liniju od posljednje zapamćene pozicije do (x, y) u globalnim koordinatama (dakle mora se izvršiti transformacija iz globalnih koordinata u koordinate canvasa i potegnuti linija uz pomoć HTML5-rutine lineTo());
+        
+        koristiBoju(c) - linija se povlači bojom c (npr. "red", "green", "blue", "black")
+        
+        povuciLiniju() – povlači liniju pozivom HTML5-rutine stroke().
+
+    Konstruktorom GKS(platno, xmin, xmax, ymin, ymax) zadaje se raspon koordinata globalnog koordinatnog sustava koji će biti prikazan u canvasu platno.
+
+*/
+
+class GKS2 {
+    unit = 60;
+    constructor(platno, xmin, xmax, ymin, ymax) {
+        this.platno = platno;
+        this.xmin = xmin;
+        this.xmax = xmax;
+        this.ymin = ymin;
+        this.ymax = ymax;
+        this.initRenderer();
+    }
+
+    initRenderer() {
+        this.renderer = this.platno.getContext("2d");
+        this.xDefault = this.platno.width / 2;
+        this.yDefault = this.platno.height / 2;
+        this.scalarX = 1;
+        this.scalarY = 1;
+    }
+
+    units(x, useXScalar = false, useYScalar = false, scaleTo640wh = false) {
+        var defaultUnitsAt640wh = this.unit;
+        var un = defaultUnitsAt640wh * ((this.platno.width > this.platno.height ? this.platno.height : this.platno.width) / 640);
+        return x * (scaleTo640wh ? un : defaultUnitsAt640wh) * (useXScalar ? this.scalarX : 1) * (useYScalar ? this.scalarY : 1);
+    }
+
+    postaviNa(x, y) {
+        this.x = x;
+        this.y = y;
+        this.renderer.beginPath();
+        this.renderer.moveTo(this.xDefault + this.units(x, true), this.yDefault + -this.units(y, false, true));
+    }
+
+    linijaDo(x, y) {
+        y = -y;
+        var xCoord = this.xDefault + this.units(x, true);
+        var yCoord = this.yDefault + this.units(y, false, true);
+
+        if (xCoord < this.xmin || xCoord > this.xmax || yCoord < this.ymin || yCoord > this.ymax) {
+            // console.log("Koordinati (" + x + ", " + y + ") su izvan granica x ε (" + this.xmin + ", " + this.xmax + ") & y ε (" + this.ymin + ", " + this.ymax + ")");
+            xCoord = Math.min(Math.max(xCoord, this.xmin), this.xmax);
+            yCoord = Math.min(Math.max(yCoord, this.ymin), this.ymax);
+        }
+
+        this.renderer.lineTo(this.xDefault + this.units(x, true), this.yDefault + this.units(y, false, true));
+    }
+
+    koristiBoju(c) {
+        this.renderer.strokeStyle = c;
+    }
+
+    povuciLiniju() {
+        this.renderer.stroke();
+    }
+}
+
+GKS2.prototype.koristiDebljinu = function(debljina) {
+    this.renderer.lineWidth = debljina;
+}
+
+GKS2.prototype.nacrtajKoordinatniSustav = function(withArrows = true, withGrid = false, withMarkers = false, maxX = 10000, maxY = 10000) {
+
+    var inicijalnaBoja = this.renderer.strokeStyle;
+    this.koristiBoju("black");
+    var inicijalnaDebljina = this.renderer.lineWidth;
+    this.koristiDebljinu(1.5);
+
+    this.nacrtajKoordinatneOsi();
+    if (withArrows)this.nacrtajStrijeliceKoordinatnogSustava();
+    if (withGrid) this.nacrtajKoordinatnuMrezu(10);
+    if (withMarkers) this.nacrtajOznake(true, maxX, maxY);
+
+    this.koristiBoju(inicijalnaBoja);
+    this.koristiDebljinu(inicijalnaDebljina);
+
+}
+
+GKS2.prototype.nacrtajKoordinatneOsi = function() {
+
+    this.postaviNa(this.xmin, 0);
+    this.linijaDo(this.xmax, 0);
+
+    this.povuciLiniju();
+
+    this.postaviNa(0, this.ymin);
+    this.linijaDo(0, this.ymax);
+
+    this.povuciLiniju();
+}
+
+GKS2.prototype.nacrtajStrijeliceKoordinatnogSustava = function() {
+
+    this.postaviNa(this.xmax, 0);
+    this.linijaDo(this.xmax - 0.3, 0.15);
+    this.povuciLiniju();
+
+    this.postaviNa(this.xmax - 0.3, -0.15);
+    this.linijaDo(this.xmax, 0);
+    this.povuciLiniju();
+
+    this.postaviNa(0, this.ymax);
+    this.linijaDo(0.15, this.ymax - 0.3);
+    this.povuciLiniju();
+
+    this.postaviNa(-0.15, this.ymax - 0.3);
+    this.linijaDo(0, this.ymax);
+    this.povuciLiniju();
+
+}
+
+GKS2.prototype.nacrtajKoordinatnuMrezu = function(expand = 0) {
+
+    var inicijalnaBoja = this.renderer.strokeStyle;
+    this.koristiBoju("lightgray");
+
+    var inicijalnaDebljina = this.renderer.lineWidth;
+    this.koristiDebljinu(1);
+
+    for (var i = this.xmin - expand; i <= this.xmax + expand; i++) {
+        this.postaviNa(i, this.ymin - expand);
+        this.linijaDo(i, this.ymax + expand);
+        this.povuciLiniju();
+    }
+
+    for (var i = this.ymin - expand; i <= this.ymax + expand; i++) {
+        this.postaviNa(this.xmin - expand, i);
+        this.linijaDo(this.xmax + expand, i);
+        this.povuciLiniju();
+    }
+
+    this.koristiDebljinu(inicijalnaDebljina);
+    this.koristiBoju(inicijalnaBoja);
+
+}
+
+GKS2.prototype.nacrtajOznake = function(enumerate = true, maxX = 10000, maxY = 10000) {
+    var inicijalnaBoja = this.renderer.strokeStyle;
+    this.koristiBoju("black");
+
+    var inicijalnaDebljina = this.renderer.lineWidth;
+    this.koristiDebljinu(1);
+
+    for (var i = this.xmin; i < this.xmax; i++) {
+        if (i == 0 || i == this.xmin) continue;
+        this.postaviNa(i, -0.1);
+        this.linijaDo(i, 0.1);
+        if (i <= maxX && i >= -maxX && enumerate) this.nacrtajSlova(i, i, -0.2);
+        this.povuciLiniju();
+    }
+
+    for (var i = this.ymin; i < this.ymax; i++) {
+        if (i == 0 || i == this.ymin) continue;
+        this.postaviNa(-0.1, i);
+        this.linijaDo(0.1, i);
+        if (i <= maxY && i >= -maxY && enumerate) this.nacrtajSlova(i, 0.2, i);
+        this.povuciLiniju();
+    }
+
+    this.koristiDebljinu(inicijalnaDebljina);
+    this.koristiBoju(inicijalnaBoja);
+};
+
+GKS2.prototype.nacrtajSlova = function(text, x, y, fontSize, fillStyle = "black") {
+    this.renderer.font = fontSize ? this.units(1) * fontSize + "px Arial" : this.units(1) / 5 + "px Arial";
+    this.renderer.fillStyle = fillStyle;
+    this.renderer.textAlign = "center";
+    this.renderer.fillText(text, this.xDefault + this.units(x, true), this.yDefault + -this.units(y - 0.0855, false, true));
+    this.renderer.textAlign = "start";
+};
+
+GKS2.prototype.displace = function(x, y) {
+    this.xDefault = this.platno.width / 2 + this.units(x);
+    this.yDefault = this.platno.height / 2 - this.units(y);
+}
+
+GKS2.prototype.placeCenterAt = function(x, y) {
+    this.xDefault = this.platno.width * x;
+    this.yDefault = this.platno.height * y;
+}

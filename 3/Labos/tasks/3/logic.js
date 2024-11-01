b3b5a26b819b82bc05b8ee77439cf33ad88e493e
Joshua Fletcher <jfletcher20@student.foi.hr>
2024-11-01 10:16:40 +0100

    Made it not speed up exponentially.

--- 3/Labos/tasks/3/logic.js
+++ 3/Labos/tasks/3/logic.js
@@ -35,29 +35,19 @@ window.onload = function() {
     var ortho = new Ortho(canvas, xmin, xmax, ymin, ymax);
     ortho.zoom = unitSlider.value;
 
-    var i = 0, k = 0, j = 0;
+    var i = 0;
     
     function draw() {
 
         canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-        
+        ortho.m.identitet();
         ortho.m.rotirajX(i);
-        ortho.m.rotirajY(k);
-        ortho.m.rotirajZ(j);
+        ortho.m.rotirajY(i);
+        ortho.m.rotirajZ(i);
 
         if (animationCheckbox.checked) {
-            i += 0.1;
-            k += 0.2;
-            j += 0.3;
-            if (i >= 360) {
-                i = 0;
-            }
-            if (k >= 360) {
-                k = 0;
-            }
-            if (j >= 360) {
-                j = 0;
-            }
+            i += 1;
+            if (i >= 360) i = 0;
         }
         drawCube("black");
 

a842d228399d812c3df59366a403c8820ccb8930
Joshua Fletcher <jfletcher20@student.foi.hr>
2024-11-01 20:34:45 +0100

    Astroid is drawn.

--- 3/Zadaća/tasks/2/logic.js
+++ 3/Zadaća/tasks/2/logic.js
@@ -44,25 +44,32 @@ window.onload = function() {
     var ortho = new Ortho(canvas, xmin, xmax, ymin, ymax);
     ortho.zoom = unitSlider.value;
 
-    function astroid(progress = 1) {
+    function astroid(r = 4, progress = 1) {
         var temp = ortho.renderer.strokeStyle;
         ortho.postaviBoju("red");
+        var x = r * Math.pow(Math.cos(t), 3);
+        var y = r * Math.pow(Math.sin(t), 3);
+        ortho.postaviNa(x, y, 0);
         for (var t = 0; t <= 2 * Math.PI * progress; t += 0.01) {
-            const x = Math.pow(Math.cos(t), 3);
-            const y = Math.pow(Math.sin(t), 3);
-            var xNext = Math.pow(Math.cos(t + 0.01), 3);
-            var yNext = Math.pow(Math.sin(t + 0.01), 3);
-            ortho.postaviNa(x, y, 0);
-            ortho.linijaDo(xNext, yNext, 0);
-            ortho.povuciLiniju();
+            x = r * Math.pow(Math.cos(t), 3);
+            y = r * Math.pow(Math.sin(t), 3);
+            ortho.linijaDo(x, y, 0);
         }
+        ortho.povuciLiniju();
+        const x2 = 3 * Math.cos(t);
+        const y2 = 3 * Math.sin(t);
+        ortho.postaviBoju(temp);
+        circle(1, -progress * 3 * 360, x2, y2, true);
+        temp = ortho.renderer.strokeStyle;
+        ortho.postaviBoju("blue");
+        circle(4, 0, 0, 0, false)
         ortho.postaviBoju(temp);
     }
 
-    function circle(r = 1, angle = 0, x = 0, y = 0) {
+    function circle(r = 1, angle = 0, x = 0, y = 0, extraStuff = true) {
         const m = new MT3D();
-        m.pomakni(rotationSliderX.value / 360, rotationSliderY.value / 360, 0);
-        m.rotirajZ(rotationSliderZ.value);
+        m.pomakni(x, y, 0);
+        m.rotirajZ(angle);
         ortho.trans(m);
         for (var t = 0; t <= 2 * Math.PI; t += 0.01) {
             const x = r * Math.cos(t);
@@ -73,18 +80,32 @@ window.onload = function() {
             ortho.linijaDo(xNext, yNext, 0);
             ortho.povuciLiniju();
         }
-        ortho.linijaDo(0, 0, 0);
-        ortho.povuciLiniju();
+        if (!extraStuff) return;
+        // circle at the end of the line
+        var temp = ortho.renderer.strokeStyle;
+        ortho.postaviBoju("red");
         for (var t = 0; t <= 2 * Math.PI; t += 0.01) {
-            const x = 0.03 * Math.cos(t);
-            const y = 0.03 * Math.sin(t);
-            var xNext = 0.03 * Math.cos(t + 0.01);
-            var yNext = 0.03 * Math.sin(t + 0.01);
+            const x = r + 0.04 * Math.cos(t);
+            const y = 0 + 0.04 * Math.sin(t);
+            var xNext = r + 0.04 * Math.cos(t + 0.01);
+            var yNext = 0 + 0.04 * Math.sin(t + 0.01);
             ortho.postaviNa(x, y, 0);
             ortho.linijaDo(xNext, yNext, 0);
             ortho.povuciLiniju();
         }
+        ortho.postaviBoju(temp);
+        ortho.postaviNa(r * Math.cos(0), r * Math.sin(0), 0);
+        ortho.linijaDo(0, 0, 0);
         ortho.povuciLiniju();
+        for (var t = 0; t <= 2 * Math.PI; t += 0.01) {
+            const x = 0.05 * Math.cos(t);
+            const y = 0.05 * Math.sin(t);
+            var xNext = 0.05 * Math.cos(t + 0.01);
+            var yNext = 0.05 * Math.sin(t + 0.01);
+            ortho.postaviNa(x, y, 0);
+            ortho.linijaDo(xNext, yNext, 0);
+            ortho.povuciLiniju();
+        }
     }
     
     function draw() {
@@ -92,10 +113,8 @@ window.onload = function() {
         ortho.initRenderer();
 
         canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-        // ortho.nacrtajKoordinatniSustav(false, false, true, 10000, 10000);
         
-        circle();
-        astroid(rotationSliderZ.value / 360);
+        astroid(4, 1 - rotationSliderZ.value / 360);
 
     }
 
